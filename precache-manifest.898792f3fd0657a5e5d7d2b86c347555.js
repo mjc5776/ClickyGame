5e50215a0146561c664eb7ca6b2694d8460cc6f8
Mike Corey <mjcorey57@gmail.com>
2019-03-10 08:28:34 -0600

    Updates

---
+++ precache-manifest.898792f3fd0657a5e5d7d2b86c347555.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2c3a1b5a67c5d677a07d",
+    "url": "/ClickyGame/static/js/runtime~main.038e769c.js"
+  },
+  {
+    "revision": "8533a546caedede5a18f",
+    "url": "/ClickyGame/static/js/main.cc9eaa5b.chunk.js"
+  },
+  {
+    "revision": "ff661de6195320cfc0eb",
+    "url": "/ClickyGame/static/js/2.822769df.chunk.js"
+  },
+  {
+    "revision": "8533a546caedede5a18f",
+    "url": "/ClickyGame/static/css/main.bc0d1392.chunk.css"
+  },
+  {
+    "revision": "e80a068b2ea16a309c6b78da63146500",
+    "url": "/ClickyGame/index.html"
+  }
+];
